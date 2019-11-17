e7f0d1ab5c3a7f6978e1c8da8d3e7efa073e3654
Sebastian Vera <u.verainf@gmail.com>
2019-11-17 22:59:07 +0100

    refactor: proxy all the props but style's related props to HeaderLink

--- src/components/Header/Link/Link.jsx
+++ src/components/Header/Link/Link.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function HomeLink({ children, to, exact = false }) {
+function HomeLink(props) {
   return (
     <NavLink
+      {...props}
       className="py-1 text-gray-700 font-medium hover:text-gray-800"
       activeClassName="text-blue-700 hover:text-blue-700 font-bold border-blue-700 border-b-2"
-      to={to}
-      exact={exact}
     >
-      {children}
+      {props.children}
     </NavLink>
   );
 }
