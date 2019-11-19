922cd8396b2727b7e65dd2000ca232e3fa80e472
Sebastian Vera <u.verainf@gmail.com>
2019-11-19 23:12:15 +0100

    feat: add a tiny border on the top of the links for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,27 +10,29 @@ function Header() {
   const closeMenu = () => setIsOpen(false);
 
   return (
-    <nav className="p-6 flex items-center justify-between flex-wrap">
-      <h1 className="text-gray-900 text-3xl">
-        <Link to="/" onClick={closeMenu}>
-          ppoker
-        </Link>
-      </h1>
-      <div className="block lg:hidden">
-        <button
-          type="button"
-          className="flex items-center px-3 py-2 border rounded text-gray-600 border-gray-500 hover:text-gray-800 hover:border-gray-900"
-          onClick={toggleMenu}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <nav className="flex items-center justify-between flex-wrap">
+      <div className="p-6 w-full lg:w-auto flex items-center justify-between flex-wrap">
+        <h1 className="text-gray-900 text-3xl">
+          <Link to="/" onClick={closeMenu}>
+            ppoker
+          </Link>
+        </h1>
+        <div className="block lg:hidden">
+          <button
+            type="button"
+            className="flex items-center px-3 py-2 border rounded text-gray-600 border-gray-500 hover:text-gray-800 hover:border-gray-900"
+            onClick={toggleMenu}
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
       </div>
       <div
         className={classnames(
@@ -38,23 +40,23 @@ function Header() {
           'w-full lg:inline-block flex-grow lg:w-2/4 lg:max-w-md',
         )}
       >
-        <ul className="flex-grow lg:flex lg:justify-between">
-          <li className="block mt-4 lg:mt-0">
+        <ul className="border-t border-gray-300 lg:border-t-0 flex-grow lg:flex lg:justify-between">
+          <li className="block ml-4 mt-4 lg:mt-0">
             <HeaderLink to="/" exact onClick={closeMenu}>
               Home
             </HeaderLink>
           </li>
-          <li className="block mt-4 lg:mt-0">
+          <li className="block ml-4 mt-4 lg:mt-0">
             <HeaderLink to="/about" onClick={closeMenu}>
               About
             </HeaderLink>
           </li>
-          <li className="block mt-4 lg:mt-0">
+          <li className="block ml-4 mt-4 lg:mt-0">
             <HeaderLink to="/contact" onClick={closeMenu}>
               Contact
             </HeaderLink>
           </li>
-          <li className="block mt-4 lg:hidden">
+          <li className="block ml-4 mt-4 lg:hidden">
             <button className="px-4 py-2 leading-none text-sm rounded border border-gray-800 focus:outline-none hover:text-white hover:bg-gray-800">
               New Room
             </button>
