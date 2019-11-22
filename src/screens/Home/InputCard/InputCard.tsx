8160419a548090bdb434138d9e297fdbe848af3f
Sebastián Vera <u.verainf@gmail.com>
2019-11-22 19:15:49 +0100

    fix: display input next to the button on mobile (#11)

--- src/screens/Home/InputCard/InputCard.tsx
+++ src/screens/Home/InputCard/InputCard.tsx
@@ -6,16 +6,16 @@ function HomeInputCard({ title, onClick, buttonLabel }: any) {
       <h2 className="mb-4 uppercase tracking-wide font-semibold text-gray-800">
         {title}
       </h2>
-      <div>
+      <div className="flex">
         <input
-          className="py-2 px-4 rounded-l border-2 bg-gray-100 border-gray-500 text-gray-700 focus:bg-white focus:outline-none focus:border-black"
+          className="py-2 px-4 border-2 bg-gray-100 rounded-l rounded-r-none border-gray-500 text-gray-700 focus:bg-white focus:outline-none focus:border-black"
           type="text"
           placeholder="new-room"
         />
         <button
           onClick={onClick}
           type="button"
-          className="py-2 px-6 w-24 border-2 border-black bg-black rounded-r font-bold focus:outline-none hover:bg-gray-900 focus:shadow-outline"
+          className="py-2 px-6 w-24 border-2 rounded-l-none border-black rounded-r bg-black font-bold focus:outline-none hover:bg-gray-900 focus:shadow-outline"
         >
           {buttonLabel}
         </button>
