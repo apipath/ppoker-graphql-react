4479b92f7a701882809683250eec98abbc8d75f3
Sebastian Vera <u.verainf@gmail.com>
2020-03-18 22:34:09 +0100

    feat: create components for the Show view

---
+++ src/screens/Room/Show/Participants/Participants.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+type Props = {
+  participants: Array<{
+    id: number | string;
+    name: string;
+    voteLabel?: string | null;
+  }>;
+  observers: Array<{
+    id: number | string;
+    name: string;
+  }>;
+  showVotes: boolean;
+};
+
+const Participants: React.FC<Props> = ({
+  participants,
+  observers,
+  showVotes,
+}) => {
+  // TODO: only display current user vote when showVotes is false
+  return (
+    <div className="w-full border border-gray-300 rounded">
+      <h3 className="py-2 font-semibold text-center text-indigo-100 bg-indigo-900 border-b border-gray-300 rounded-tl rounded-tr">
+        Participants
+      </h3>
+      <ul className="p-4">
+        {participants.map(participant => (
+          <li key={participant.id} className="flex justify-between">
+            <div>{participant.name}</div>
+            <div>
+              {showVotes
+                ? participant.voteLabel
+                : participant.voteLabel
+                ? checkIcon
+                : clockIcon}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <h3 className="py-2 text-center border-t border-b border-gray-300">
+        Observers
+      </h3>
+      <ul className="p-4">
+        {observers.map(observer => (
+          <li key={observer.id}>{observer.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const clockIcon = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 8V12L15 15M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const checkIcon = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5 13L9 17L19 7"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default Participants;
--- src/screens/Room/Show/Show.tsx
+++ src/screens/Room/Show/Show.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import cn from 'classnames';
 import Button from '../../../components/Button';
+import Participants from './Participants';
+import PointCard from './PointCard';
+import Results from './Results';
 
 type Room = {
   id: string;
@@ -36,38 +39,19 @@ const mockedRoom: Room = {
 };
 
 const mockedParticipants = [
-  { id: 1, name: 'Foo', observer: false, voteLabel: '8' },
-  { id: 2, name: 'React', observer: false, voteLabel: '3' },
-  { id: 3, name: 'Bar', observer: false, voteLabel: '8' },
-  { id: 4, name: 'Elixir', observer: false, voteLabel: '3' },
-  { id: 5, name: 'Baz', observer: false, voteLabel: '8' },
-  { id: 6, name: 'Golang', observer: false, voteLabel: '1' },
-  { id: 7, name: 'GraphQL', observer: false, voteLabel: '8' },
+  { id: 1, name: 'Foo', voteLabel: '8' },
+  { id: 2, name: 'React', voteLabel: undefined },
+  { id: 3, name: 'Bar', voteLabel: '8' },
+  { id: 4, name: 'Elixir', voteLabel: '3' },
+  { id: 5, name: 'Baz', voteLabel: '8' },
+  { id: 6, name: 'Golang', voteLabel: '1' },
+  { id: 7, name: 'GraphQL', voteLabel: '8' },
 ];
 
 const mockedObservers = [
-  { id: 8, name: 'Observer 1', observer: true },
-  { id: 9, name: 'Observer 2', observer: true },
+  { id: 8, name: 'Observer 1' },
+  { id: 9, name: 'Observer 2' },
 ];
-
-type CardProps = {
-  point: Point;
-};
-
-const Card: React.FC<CardProps> = ({ point }) => {
-  return (
-    <div className="flex flex-col w-40 h-40 p-2 bg-white rounded shadow-md cursor-pointer hover:shadow-lg p4">
-      <header className="flex items-center justify-center flex-grow">
-        <h3 className="text-4xl">{point.label}</h3>
-      </header>
-      <div className="text-xs">
-        <span className="px-2 py-1 bg-blue-100 rounded-full">
-          {point.description}
-        </span>
-      </div>
-    </div>
-  );
-};
 
 function RoomShow() {
   const { id } = useParams();
@@ -75,26 +59,7 @@ function RoomShow() {
   const [room] = useState(mockedRoom);
   const [participants] = useState(mockedParticipants);
   const [observers] = useState(mockedObservers);
-  const pointsById = room.points.reduce(
-    (obj: { [key: string]: Point }, point) => {
-      obj[point.label] = point;
-      return obj;
-    },
-    {},
-  );
-
-  // TODO: only when there's no consensus
-  const statistics = participants.reduce(
-    (obj: { [key: string]: number }, participant) => {
-      obj[participant.voteLabel] = obj[participant.voteLabel] || 0;
-      obj[participant.voteLabel]++;
-      return obj;
-    },
-    {},
-  );
-  const sortedStatistics = Object.entries(statistics).sort(
-    ([, a], [, b]) => b - a,
-  );
+  const [showVotes] = useState(false);
 
   return (
     <section className="p-4 lg:p-5">
@@ -106,72 +71,23 @@ function RoomShow() {
         <ul className={cn('w-full grid gap-2 grid-cols-fill-40', 'lg:w-1/2')}>
           {room.points.map(point => (
             <li className="flex justify-center" key={point.label}>
-              <Card point={point} />
+              <PointCard point={point} />
             </li>
           ))}
         </ul>
         <div className="flex-grow mt-6 lg:mt-0">
           <div className="flex flex-col w-full md:grid md:grid-cols-2 md:gap-4">
-            <div className="w-full border border-gray-300 rounded">
-              <h3 className="py-2 font-semibold text-center text-indigo-100 bg-indigo-900 border-b border-gray-300 rounded-tl rounded-tr">
-                Participants
-              </h3>
-              <ul className="p-4">
-                {participants.map(participant => (
-                  <li key={participant.id} className="flex justify-between">
-                    <div>{participant.name}</div>
-                    <div>{pointsById[participant.voteLabel].label}</div>
-                  </li>
-                ))}
-              </ul>
-              <h3 className="py-2 text-center border-t border-b border-gray-300">
-                Observers
-              </h3>
-              <ul className="p-4">
-                {observers.map(observer => (
-                  <li key={observer.id}>{observer.name}</li>
-                ))}
-              </ul>
-            </div>
+            <Participants
+              participants={participants}
+              observers={observers}
+              showVotes={showVotes}
+            />
             <div className="w-full mt-6 md:mt-0">
               <div className="flex justify-between mx-12 mb-12">
                 <Button>Show Votes</Button>
                 <Button>Clear Votes</Button>
               </div>
-              <div className="flex flex-col mb-6 text-xl ">
-                <span className="text-4xl text-center">8</span>
-                <div className="flex justify-center">
-                  <span className="px-4 py-1 text-white uppercase bg-green-400 rounded-full">
-                    Consensus
-                  </span>
-                </div>
-              </div>
-              <div className="flex justify-center mb-6 bg-white rounded shadow">
-                <table className="w-full text-left border-collapse">
-                  <thead>
-                    <tr>
-                      <th className="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                        Point
-                      </th>
-                      <th className="px-6 py-4 text-sm font-bold uppercase border-b bg-grey-lightest text-grey-dark border-grey-light">
-                        Value
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {sortedStatistics.map(([label, count]) => (
-                      <tr key={label}>
-                        <td className="px-6 py-4">{label}</td>
-                        <td className="flex items-center px-6 py-4">
-                          <span className="text-indigo-100 rounded-full bg-indigo-500 uppercase px-4 py-1 text-xs font-bold mr-3">
-                            {count}
-                          </span>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+              <Results participants={participants} />
             </div>
           </div>
         </div>
