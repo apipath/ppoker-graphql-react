78c26d59bea452b4cbd97987fd4b9a2aebdd94d8
Sebastian Vera <u.verainf@gmail.com>
2020-03-18 22:01:39 +0100

    feat: add border bottom on label input on focus

--- src/screens/Room/Create/Create.tsx
+++ src/screens/Room/Create/Create.tsx
@@ -38,7 +38,7 @@ function PointRow({
       <div className="mr-2">
         <input
           ref={labelRef}
-          className="p-2 rounded outline-none"
+          className="p-2 border-b-2 border-gray-400 outline-none focus:border-teal-500"
           placeholder="label"
           value={label}
           onChange={handleLabelChange}
@@ -46,7 +46,7 @@ function PointRow({
       </div>
       <div>
         <input
-          className="p-2 font-semibold text-center text-blue-500 placeholder-blue-300 bg-blue-100 rounded-full outline-none"
+          className="p-2 font-medium text-center text-blue-400 placeholder-blue-300 bg-blue-100 rounded-full outline-none focus:placeholder-transparent"
           placeholder="description"
           value={description}
           onChange={handleDescriptionChange}
