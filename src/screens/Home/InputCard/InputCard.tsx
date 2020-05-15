cc120c0b3174b7b9f5a457e929d1f8e23ca6677b
Sebastián Vera <u.verainf@gmail.com>
2020-05-15 11:41:01 +0200

    fix: add autocomplete to join room input in the Home component (#36)

--- src/screens/Home/InputCard/InputCard.tsx
+++ src/screens/Home/InputCard/InputCard.tsx
@@ -2,7 +2,7 @@ import React, { MouseEventHandler, HTMLProps } from 'react';
 
 type Props = {
   title: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  onSubmit: MouseEventHandler<HTMLButtonElement>;
   buttonLabel: string;
   value: string;
 } & HTMLProps<HTMLInputElement>;
@@ -11,7 +11,7 @@ function HomeInputCard({
   title,
   buttonLabel,
   value,
-  onClick,
+  onSubmit,
   ...inputProps
 }: Props) {
   return (
@@ -19,7 +19,7 @@ function HomeInputCard({
       <h2 className="mb-4 font-semibold tracking-wide text-gray-800 uppercase">
         {title}
       </h2>
-      <div className="flex">
+      <form onSubmit={(e) => e.preventDefault()} className="flex">
         <input
           {...inputProps}
           className="px-4 py-2 text-gray-700 bg-gray-100 border-2 border-gray-500 rounded-l rounded-r-none focus:bg-white focus:outline-none focus:border-black"
@@ -29,13 +29,13 @@ function HomeInputCard({
           value={value}
         />
         <button
-          onClick={onClick}
+          onClick={onSubmit}
           type="button"
           className="py-2 px-6 w-24 border-2 rounded-l-none border-black rounded-r bg-black font-bold focus:outline-none hover:bg-gray-900 focus:shadow-outline"
         >
           {buttonLabel}
         </button>
-      </div>
+      </form>
     </div>
   );
 }
