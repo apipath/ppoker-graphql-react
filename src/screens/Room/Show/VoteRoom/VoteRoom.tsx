a520611d66f84f92141923b2bb6b2b9457d5f647
Sebastian Vera <u.verainf@gmail.com>
2020-10-31 21:30:09 +0100

    fix: align elements in observer view

--- src/screens/Room/Show/VoteRoom/VoteRoom.tsx
+++ src/screens/Room/Show/VoteRoom/VoteRoom.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cn from 'classnames';
 import { useToasts } from 'react-toast-notifications';
 import { AnimatePresence, motion } from 'framer-motion';
 
@@ -127,33 +126,35 @@ const VoteRoom: React.FC<Props> = ({ room, user }) => {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row">
-      <ul
-        className={cn('w-full grid gap-2 grid-cols-fill-40 mb-4', 'lg:w-1/2')}
-      >
-        {room.points.map((point) => (
-          <li
-            onClick={() =>
-              user.role === Role.Participant &&
-              !voteLoading &&
-              handleClick(point)
-            }
-            className="flex justify-center"
-            key={point.label}
-          >
-            <PointCard
-              point={point}
-              disabled={user.role === Role.Observer || voteLoading}
-              selected={point.label === selectedPoint}
-            />
-          </li>
-        ))}
-      </ul>
-      {user.role === Role.Observer && (
-        <h3 className="text-sm text-center text-gray-600 leading-5">
-          Observers cannot vote
-        </h3>
-      )}
+    <div className="flex flex-col flex-col-reverse lg:flex-row">
+      <div className="flex flex-col-reverse lg:flex-col lg:w-1/2">
+        <ul className="w-full grid gap-2 grid-cols-fill-40">
+          {room.points.map((point) => (
+            <li
+              onClick={() =>
+                user.role === Role.Participant &&
+                !voteLoading &&
+                handleClick(point)
+              }
+              className="flex justify-center"
+              key={point.label}
+            >
+              <PointCard
+                point={point}
+                disabled={user.role === Role.Observer || voteLoading}
+                selected={point.label === selectedPoint}
+              />
+            </li>
+          ))}
+        </ul>
+        {user.role === Role.Observer && (
+          <div className="block mb-4 lg:mt-4">
+            <h3 className="text-sm text-center text-gray-600 leading-5">
+              Observers cannot vote
+            </h3>
+          </div>
+        )}
+      </div>
       <div className="my-4 border-b border-gray-300 lg:hidden"></div>
       <div className="flex-grow mt-4 lg:mt-0">
         <div className="flex flex-col w-full md:grid md:grid-cols-2 md:gap-4">
