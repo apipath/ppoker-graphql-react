b36c80549da5d678f60cfdaf63d7d66496449222
Sebastian Vera <u.verainf@gmail.com>
2020-11-01 00:17:10 +0100

    fix: avoid creating dynamic css classes to avoid purging them

--- src/components/Button3D/Button3D.tsx
+++ src/components/Button3D/Button3D.tsx
@@ -5,6 +5,13 @@ import cn from 'classnames';
 
 type Color = 'blue' | 'purple' | 'teal';
 
+const COLORS: Record<Color, string> = {
+  purple:
+    'bg-purple-500 border-purple-700 hover:bg-purple-400 hover:border-purple-500',
+  blue: 'bg-blue-500 border-blue-700 hover:bg-blue-400 hover:border-blue-500',
+  teal: 'bg-teal-500 border-teal-700 hover:bg-teal-400 hover:border-teal-500',
+};
+
 type Props = React.PropsWithoutRef<JSX.IntrinsicElements['button']> & {
   loading?: boolean;
   color?: Color;
@@ -17,10 +24,7 @@ const Button3D: React.FC<Props> = ({ loading, color = 'blue', ...props }) => {
       {...props}
       disabled={disabled}
       className={cn('px-4 py-2 font-bold text-white border-b-4 rounded', {
-        ['bg-purple-500 border-purple-700 hover:bg-purple-400 hover:border-purple-500'.replaceAll(
-          'purple',
-          color,
-        )]: !disabled,
+        [COLORS[color]]: !disabled,
         'bg-gray-100 text-gray-400 opacity-60 cursor-not-allowed': disabled,
         'hover:text-gray-200 text-white': !disabled,
       })}
--- src/components/Link3D/Link3D.tsx
+++ src/components/Link3D/Link3D.tsx
@@ -4,6 +4,14 @@ import cn from 'classnames';
 
 type Color = 'purple' | 'blue' | 'indigo';
 
+const COLORS: Record<Color, string> = {
+  purple:
+    'bg-purple-500 border-purple-700 hover:bg-purple-400 hover:border-purple-500',
+  blue: 'bg-blue-500 border-blue-700 hover:bg-blue-400 hover:border-blue-500',
+  indigo:
+    'bg-indigo-500 border-indigo-700 hover:bg-indigo-400 hover:border-indigo-500',
+};
+
 const Link3D: React.FC<LinkProps & { color?: Color }> = ({
   color = 'purple',
   className,
@@ -14,10 +22,7 @@ const Link3D: React.FC<LinkProps & { color?: Color }> = ({
       {...rest}
       className={cn(
         'px-4 py-2 font-bold text-white border-b-4 rounded',
-        'bg-purple-500 border-purple-700 hover:bg-purple-400 hover:border-purple-500'.replaceAll(
-          'purple',
-          color,
-        ),
+        COLORS[color],
         className,
       )}
     />
