81a5b7ee277d023881dd86da2f47cf4ae68c45fc
Sebastián Vera <u.verainf@gmail.com>
2020-11-23 15:11:02 +0100

    Feat/better about page (#66)
    * feat: new about page for desktop
    
    * feat: update about page for mobile
    
    * fix: remove flex-gap since it has almost no support

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -1,49 +1,53 @@
 import React from 'react';
+import cn from 'classnames';
 
 import members from './members';
 import AboutMember from './Member';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 
 const About: React.FC = () => {
   return (
     <>
       <Header />
-      <article>
-        <section className="flex flex-col items-center text-center">
-          <h2 className="text-4xl font-semibold tracking-wide text-gray-800">
-            About us
+      <article className="mb-12">
+        <section className="flex flex-col items-center justify-center py-8">
+          <h2 className="text-3xl font-semibold text-gray-800">
+            Meet the team
           </h2>
-          <p className="max-w-4xl m-8 text-xl">
+          <p className="max-w-lg p-4 text-xl text-center text-gray-700">
             We are a group of passionate developers who met over time and
             decided to share what we have learned by building apps.
           </p>
         </section>
 
-        <section className="mt-4 text-center">
-          <h2 className="text-4xl font-semibold tracking-wide text-gray-800">
-            Meet the team
-          </h2>
-          <div className="flex justify-center mt-4">
-            <ul className="flex flex-wrap justify-center">
-              {members.map(({ name, login, location, htmlUrl }) => (
-                <li
-                  key={name}
-                  className="flex justify-center w-full sm:my-4 sm:flex sm:justify-center sm:w-1/2 md:w-5/12 lg:w-1/3"
-                >
-                  <AboutMember
-                    name={name}
-                    login={login}
-                    location={location}
-                    htmlUrl={htmlUrl}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
+        <section className="flex justify-center">
+          <ul
+            className={cn(
+              'flex flex-wrap items-center justify-around',
+              'max-w-3xl',
+            )}
+          >
+            {members.map((props, index) => (
+              <li
+                key={props.name}
+                className={cn(
+                  'transition-all',
+                  'transform motion-reduce:transform-none ease-out duration-300',
+                  'hover:rotate-0 hover:scale-110',
+                  {
+                    'rotate-3 mx-8': index === 0,
+                    '-rotate-3 mt-8 sm:mt-2 sm:ml-8': index === 1,
+                    'rotate-3 mt-8 sm:-rotate-3 sm:mt-8': index === 2,
+                    '-rotate-3 mt-8 sm:rotate-6 sm:mt-12': index === 3,
+                  },
+                )}
+              >
+                <AboutMember {...props} />
+              </li>
+            ))}
+          </ul>
         </section>
       </article>
-      <Footer />
     </>
   );
 };
